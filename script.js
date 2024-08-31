c7e6401066271093de35b5e687540ff2b4fb9c22
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2024-08-31 14:41:45 -0500

    Fixed typo in HTML, added logic for operation function and fixed typos in numberButtons function and reworked logic and added comments for clarifcation

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-let num1 = 0;
-let num2 = 0;
-let operator = " ";
-let equalsClicked = false;
+let num1 = "";
+let num2 = "";
+let operator = "";
+let equalClicked = false;
 
-const display = document.getElementById("dispalyOutput");
+const display = document.getElementById("displayOutput");
 const numberButtons = document.querySelectorAll(".digit");
 const operatorButtons = document.querySelectorAll(".operator");
 const equalsButton = document.querySelector(".equals");
 const clearButton = document.querySelector(".clear");
 
-function operate(num1, num2, operator) {}
+// takes numbers and operator and calculates
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      if (num2 === 0) {
+        return "Error: Division by zero";
+      }
+      return num1 / num2;
+    default:
+      return "Error: Invalid operator";
+  }
+}
 
 /*
 After the number is displayed into the display store that number into num1
@@ -19,20 +36,28 @@ After the equal signs is clicked run the operate command to do the calculation
 Possible use a switch case?
 */
 
+/*
+Whatever number is pressed their text content eg: 1 or 2 or 3 will be stores into the const variable
+If the equal button was already clicked then the display will show the number pressed and set the variable to false
+If the equal button was not clicked then the number will be added to the display
+*/
 numberButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
     const buttonNumValue = event.target.textContent;
 
-    if (display.textContent === "0") {
+    if (equalClicked) {
       display.textContent = buttonNumValue;
+      equalClicked = false;
     } else {
-      display.textContent += buttonNumber + " ";
+      display.textContent === "0" //Check to see if the display is showing 0
+        ? (display.textContent = buttonNumValue) // if yes then makes the display output equal the number pressed
+        : (display.textContent += buttonNumValue); // if no then adds that number onto the numbers already present
     }
 
     if (operator) {
-      num2 += buttonNumber;
+      num2 += buttonNumValue; // if the operator being clicked is true then store the nubmer in num2
     } else {
-      num1 += buttonNumber;
+      num1 += buttonNumValue; // if the operator being clicked is false then store in num1
     }
   });
 });
